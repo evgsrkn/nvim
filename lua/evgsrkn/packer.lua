31dc8df10e462de81c27c1dd5445e3b521cef946
Evgeniy <evgsrkn@gmail.com>
2024-02-15 23:29:39 +0300

    harpoon v2

--- lua/evgsrkn/packer.lua
+++ lua/evgsrkn/packer.lua
@@ -81,6 +81,7 @@ return require('packer').startup(function(use)
   use("nvim-treesitter/nvim-treesitter-context")
   use {
     'theprimeagen/harpoon',
+    branch = "harpoon2",
     requires = { {"nvim-lua/plenary.nvim"} }
   }
   use {
